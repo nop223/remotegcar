c74136c07ddf40f247f8db0bf738c55bb999266a
nop223 <62751776+nop223@users.noreply.github.com>
2020-10-18 17:02:56 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,9 @@
-input.onButtonPressed(Button.A, function () {
-    control2 = 0
-    basic.showNumber(control2)
-})
-input.onButtonPressed(Button.B, function () {
-    if (control2 == 0) {
-        control2 = 1
-    } else {
-        control2 = control2 + 1
-        if (control2 > 6) {
-            control2 = 1
-        }
-    }
-    basic.showNumber(control2)
-})
 let serdir = 0
 let yx = 0
 let xx = 0
-let control2 = 0
 radio.setGroup(0)
 let Ax = 9
-control2 = 0
+let control2 = 0
 let strt = 0
 basic.forever(function () {
     xx = pins.map(
@@ -49,15 +33,5 @@ basic.forever(function () {
     if (control2 == 0) {
         radio.sendValue("MotorX", xx)
         radio.sendValue("MotorY", yx)
-    } else {
-        if (control2 > 0 && serdir > 0) {
-            if (strt == 1) {
-                radio.sendValue("sernr", control2)
-                radio.sendValue("serdir", serdir)
-                strt = 0
-            }
-        } else {
-        	
-        }
     }
 })
